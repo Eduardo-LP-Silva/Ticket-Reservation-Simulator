ee635e3706f7fb8c1bb667c5e9c59a6ab7773ceb
EdS <eduardo__lps@hotmail.com>
2018-05-04 22:43:10 +0100

    Created general main structure

--- server.c
+++ server.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include "server.h"
 
+void *reserve_seats(void *arg)
+{
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -17,13 +14,29 @@ int main(int argc, char *argv[])
     }
 
     int num_room_seats = atoi(argv[1]), num_ticket_offices = atoi(argv[2]), 
-        open_time = atoi(argv[3]), *requests;
+        open_time = atoi(argv[3]);
 
     mkfifo("requests", 770);
 
+    int t, ticket_office;
+    pthread_t tids[num_ticket_offices];
+
+    for(t = 0; t < num_ticket_offices; t++)
+        ticket_office = pthread_create(&tids[t], NULL, reserve_seats, NULL);
+
+    int requests = open("requests", O_RDONLY);
+    int *order[1];
+    clock_t start;
+
+    while((double) (clock() - start) / CLOCKS_PER_SEC <= open_time)
+        read(requests, order[0], (MAX_CLI_SEATS + 2) * sizeof(int));
+
+    for(t = 0; t < num_ticket_offices; t++)
+        pthread_kill(tids[t], SIGINT);
+
+    close(requests);
     
-
-
     unlink("requests");
 
+    return 0;
 }
--- server.h
+++ server.h
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <time.h>
+#include <signal.h>
+
 #define MAX_ROOM_SEATS 9999
 #define MAX_CLI_SEATS 99
 #define WIDTH_PID 5
