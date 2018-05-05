b64a22632ec846dc1f314dbe45696ccba6e34688
helenaMontenegro <up201604184@fe.up.pt>
2018-05-05 17:25:54 +0100

    added created threads and some functions

--- server.c
+++ server.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include "server.h"
-
 
 int main(int argc, char *argv[])
 {
@@ -17,13 +9,79 @@ int main(int argc, char *argv[])
     }
 
     int num_room_seats = atoi(argv[1]), num_ticket_offices = atoi(argv[2]), 
-        open_time = atoi(argv[3]), *requests;
+        open_time = atoi(argv[3]), *requests; //TODO: initialize requests!!
+
+    if(num_room_seats < 0 || num_ticket_offices < 0 || open_time < 0)
+    {
+    	printf("Invalid arguments\n");
+    	return -1;
+    }
+
+    struct Seat* seats = createSeats(num_room_seats);
+
+    //TESTES
+/*
+    printf("%d\n", isSeatFree(seats, 1));
+    bookSeat(seats, 1, 235);
+    printf("%d\n", isSeatFree(seats, 1));
+    freeSeat(seats, 1);
+    printf("%d\n", isSeatFree(seats, 1));*/
 
     mkfifo("requests", 770);
 
-    
+    //creating threads
+    pthread_t tid[num_ticket_offices];
 
+    for(int i = 0; i < num_ticket_offices; i++)
+    {
+    	int err = pthread_create(&tid[i], NULL, handleReservations, requests);
+    	if(err)
+    	{
+    		printf("Error creating thread");
+    		exit(1);
+    	}
+    }
 
     unlink("requests");
+    free(seats);
 
 }
+
+struct Seat* createSeats(int numSeats)
+{
+	struct Seat* seats = malloc(numSeats*sizeof(struct Seat));
+	for(int i = 0; i < numSeats; i++)
+	{
+		(seats+i*(sizeof(struct Seat)))->numSeat = i+1;
+		(seats+i*(sizeof(struct Seat)))->clientId = -1;
+	}
+	return seats;
+}
+
+void *handleReservations(void *arg)
+{
+	//TODO: read from fifo; check if request is valid; if so, execute it
+	return NULL;
+}
+
+int isSeatFree(struct Seat *seats, int seatNum)
+{
+	int isFree = 0;
+	if((seats+(seatNum-1)*sizeof(struct Seat))->clientId < 0)
+		isFree = 1;
+	DELAY();
+	return isFree;
+}
+
+void bookSeat(struct Seat *seats, int seatNum, int clientId)
+{
+	(seats+(seatNum-1)*sizeof(struct Seat))->clientId = clientId;
+	DELAY();
+}
+
+void freeSeat(struct Seat *seats, int seatNum)
+{
+	(seats+(seatNum-1)*sizeof(struct Seat))->clientId = -1;
+	DELAY();
+}
+
--- server.h
+++ server.h
@@ -1,7 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #define MAX_ROOM_SEATS 9999
 #define MAX_CLI_SEATS 99
 #define WIDTH_PID 5
 #define WIDTH_XXNN 5
 #define WIDTH_SEAT 4
 
+#define DELAY()
 
+struct Seat{
+	int numSeat;
+	int clientId; //if client_pid < 0 -> seat is free
+};
+
+void *handleReservations(void *arg);
+int isSeatFree(struct Seat *seats, int seatNum);
+void bookSeat(struct Seat *seats, int seatNum, int clientId);
+void freeSeat(struct Seat *seats, int seatNum);
+struct Seat* createSeats(int numSeats);
